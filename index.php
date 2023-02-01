497dccf1c355fae94bee5c6e2e3edd7663b5c320
Saraesp <saraesposito145@gmail.com>
2023-02-01 20:36:09 +0100

    Correzione Milestone 1

--- index.php
+++ index.php
@@ -1,67 +1,69 @@
 <?php
-    function generateRanPassword($lenght){
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:\<>,.?/\\;';
+    function generateRanPassword($lenght)
+    {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:\<,.?/\\;';
 
-        $password = '';
+        $pwd = '';
 
-        while(strlen($password) < $lenght){
-            $randomChar = $alphabet[randomIndex($alphabet)];
-            $password .= $randomChar;
+        while(strlen($pwd) < $lenght){
+            $shfl = $alphabet[rand(0, strlen($alphabet) -1)];
+            $pwd .=  $shfl;
         }
-        var_dump($password);
+        return $pwd;
     }
 
     $error = '';
-    if(isset($_GET['lungh-password']) && $_GET['lungh-password'] !== ''){
-                var_dump($_GET['lungh-password']);
-        $error = 'La lunghezza della password non è valida';
+    if(isset($_GET['lungh_password']) && $_GET['lungh_password'] !== ''){
+        
+        $pwd = generateRanPassword($_GET['lungh_password']);
     }
-    elseif(isset($_GET['lungh-password']) && $_GET['lungh-password'] !== ''){
-        generateRanPassword($_GET['lungh-password']);
+    elseif(isset($_GET['lungh_password']) && $_GET['lungh_password'] === ''){
+        
+        $error = 'Non hai inserito un valore per la lunghezza';
     }
 
-    // $password = generateRanPassword($_GET['lungh-password']);
+   
     
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./css/style.css">
-</head>
-<body>
-        <div class="container bg-blue">
-            <div class="text-center">
-                 <h1 class="text-secondary">Strong Password Generator</h1>
-                <h3 class="text-white">Genera una password sicura</h3>
-            </div>
-            <?php if(isset($error)){ ?>
-                <div class="alert alert-primary" role="alert">
-                    <?php echo $error ?>
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+            <div class="container bg-blue">
+                <div class="text-center">
+                    <h1 class="text-secondary">Strong Password Generator</h1>
+                    <h3 class="text-white">Genera una password sicura</h3>
                 </div>
-            <?php } ?>
-            <?php if(isset($password)){ ?>
-                <div class="alert alert-success" role="alert">
-                   <p>La password generata è: <strong><?php echo $password ?></strong> </p> 
-                </div>
-            <?php } ?>
-            <div class="bg-white p-4 mt-5">
-                <form action="./index.php" method="GET">
-                    <div class="row">
-                        <label class="col-sm-4 col-form-label">Lunghezza Password:</label>
-                        <div class="col-sm-auto">
-                            <input type="number" name="lungh-password" class="form-control">
-                        </div>
-                         <button type="submit" class="btn btn-primary col-sm-auto">Invia</button>
+                <?php if($error !== ''){ ?>
+                    <div class="alert alert-primary" role="alert">
+                        <?php echo $error ?>
                     </div>
-                </form>
+                <?php } ?>
+                <?php if(isset($pwd)){ ?>
+                    <div class="alert alert-success" role="alert">
+                        <p>La password generata è: <strong><?php echo $pwd ?></strong></p> 
+                    </div>
+                <?php } ?>
+                <div class="bg-white p-4 mt-5">
+                    <form action="./index.php" method="GET">
+                        <div class="row">
+                            <label class="col-sm-4 col-form-label">Lunghezza Password:</label>
+                            <div class="col-sm-auto">
+                                <input type="number" name="lungh_password" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary col-sm-auto">Invia</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-</body>
+    </body>
 </html>
